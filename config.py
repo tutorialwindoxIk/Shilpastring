a4035878bc69ab3d46f9e0ebb6e5c724631d5f7e
Ishu_official <somnathhor12052005@gmail.com>
2024-11-05 10:48:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID","21803165"))
 API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
 
 BOT_TOKEN = getenv("BOT_TOKEN","7732529579:AAFHBwOo1tsX5rqUToaRcCwgG_iilgRv2bE")
-OWNER_ID = int(getenv("OWNER_ID","7403621976"))
+OWNER_ID = int(getenv("OWNER_ID","7070591202"))
 
-MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-MUST_JOIN = getenv("MUST_JOIN","WORLD_ALPHA")
+MONGO_DB_URI = getenv("mongodb+srv://Somu:Somu@somu.xbkiklu.mongodb.net/?retryWrites=true&w=majority&appName=Somu")
+MUST_JOIN = getenv("MUST_JOIN","somueditingzone")
